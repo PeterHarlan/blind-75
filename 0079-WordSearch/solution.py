cf37fcfd9bc2e316ca3953574bacaa2d626757ec
Peter Harlan <peterjharlan@gmail.com>
2024-08-02 16:28:53 -0500

    word search

--- 0079-WordSearch/solution.py
+++ 0079-WordSearch/solution.py
@@ -1,33 +1,85 @@
+from typing import List
+
+
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        def backtrack(i: int, j: int, idx: int) -> bool:
-            if i < 0 or i >= m or j < 0 or j >= n:
+    def exist2(self, board: List[List[str]], word: str) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+        path = set()
+
+        def dfs(r, c, i):
+            # if the index is the very end, we have found the word
+            if i == len(word):
+                return True
+
+            # When we have not found the word
+            if (
+                r < 0
+                or c < 0
+                or r >= ROWS
+                or c >= COLS
+                or word[i] != board[r][c]
+                or (r, c) in path
+            ):
                 return False
-            if (i, j) in visited:
-                return False
-            visited.add((i, j))
-            if board[i][j] == word[idx]:
-                if idx == last:
+
+            path.add((r, c))
+            res = (
+                dfs(r + 1, c, i + 1)
+                or dfs(r - 1, c, i + 1)
+                or dfs(r, c + 1, i + 1)
+                or dfs(r, c - 1, i + 1)
+            )
+            # No longer visiting that position
+            path.remove((r, c))
+            return res
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0):
                     return True
-                for d in directions:
-                    if backtrack(i + d[0], j + d[1], idx + 1):
-                        return True
-            visited.remove((i, j))
-            return False
+        return False
 
-        m = len(board)
-        n = len(board[0])
-        last = len(word) - 1
-        directions = ((-1, 0), (0, 1), (1, 0), (0, -1))  # N, E, S, W
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+        path = set()
 
-        for i in range(m):
-            for j in range(n):
-                visited = set()
-                if backtrack(i, j, 0):
+        def dfs(r, c, i):
+
+            if i == len(word):
+                return True
+
+            if (
+                r < 0
+                or c < 0
+                or r >= ROWS
+                or c >= COLS
+                or word[i] != board[r][c]
+                or (r, c) in path,
+            ):
+                return False
+
+            path.add((r, c))
+            res = (
+                dfs(r + 1, c, i + 1)
+                or dfs(r - 1, c, i + 1)
+                or dfs(r, c + 1, i + 1)
+                or dfs(r, c - 1, i + 1)
+            )
+            path.remove((r, c))
+            return res
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0):
                     return True
         return False
 
 
 if __name__ == "__main__":
+    board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
+    word = "ABCCED"
     s = Solution()
-
+    exists = s.exist(board, word)
+    print(exists)
+    # O(n*m * 4^n)
+    # dfs is called 4 times, and n is the letter of the word
