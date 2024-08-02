99a0f621434b85a63f79e69502f93bfe6bf5228c
Peter Harlan <peterjharlan@gmail.com>
2024-08-01 19:17:52 -0500

    Update code

--- 0001-TwoSum/solution.py
+++ 0001-TwoSum/solution.py
@@ -4,14 +4,16 @@ from typing import List
 class Solution:
 
     def two_sum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-        for i, n in enumerate(nums):
-            complement = target - n
-            if complement in d:
-                j = d[complement]
-                return [i, j]
+        # Key is the complement value and the value is the index
+        comp_to_index_map = {}
+        for index, number in enumerate(nums):
+            # Calculate complement value
+            complement = target - number
+            if complement in comp_to_index_map:
+                stored_index = comp_to_index_map[complement]
+                return [index, stored_index]
             else:
-                d[n] = i
+                comp_to_index_map[number] = index
 
 
 if __name__ == "__main__":
