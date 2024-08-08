2fb0bdb686762a3d943ed5e7c0c02d5519b33055
Peter Harlan <peterjharlan@gmail.com>
2024-08-07 23:14:06 -0500

    Update two sum

--- 0001-TwoSum/solution.py
+++ 0001-TwoSum/solution.py
@@ -1,5 +1,13 @@
 from typing import List
 
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
 
 class Solution:
 
@@ -18,3 +26,7 @@ class Solution:
 
 if __name__ == "__main__":
     s = Solution()
+    nums = [2, 7, 11, 15]
+    target = 9
+    out = s.two_sum(nums, target)
+    print(out)
