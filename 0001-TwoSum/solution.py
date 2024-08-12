06fe3bbf36192a4a69748cec3037afd47bb34626
Peter Harlan <peterjharlan@gmail.com>
2024-08-12 14:02:07 -0500

    Add pointers solution

--- 0001-TwoSum/solution.py
+++ 0001-TwoSum/solution.py
@@ -11,7 +11,7 @@ from typing import List
 
 class Solution:
 
-    def two_sum(self, nums: List[int], target: int) -> List[int]:
+    def two_sum_map(self, nums: List[int], target: int) -> List[int]:
         # Key is the complement value and the value is the index
         comp_to_index_map = {}
         for index, number in enumerate(nums):
@@ -23,10 +23,23 @@ class Solution:
             else:
                 comp_to_index_map[number] = index
 
+    def two_sums_pointers(self, nums: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            sum = nums[left] + nums[right]
+            if sum == target:
+                return [left, right]
+            elif sum < target:
+                left += 1
+            else:
+                right -= 1
+
 
 if __name__ == "__main__":
     s = Solution()
     nums = [2, 7, 11, 15]
     target = 9
-    out = s.two_sum(nums, target)
+    # out = s.two_sum_map(nums, target)
+    out = s.two_sums_pointers(nums, target)
     print(out)
